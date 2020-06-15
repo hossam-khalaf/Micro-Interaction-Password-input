ac20350cfe0f5db9afdbd1e4b31a53fce54ed957
hossam-khalaf <hossam.khalaf@med.au.edu.eg>
2020-06-15 16:40:41 +0200

    added very simple validaion to the password and updating the msg

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const eye = document.querySelector('.eye');
 
 action.addEventListener('click', () => {
   const root = document.documentElement;
+  const Password = document.getElementById('password');
+
   root.style.setProperty('--scale', 'scaleX(1)');
-  action.dataset.content = 'Invalid password';
+  root.style.setProperty('--opacity', 1);
+
+  if (Password.value === 'password') {
+    action.dataset.content = 'Welcome Back';
+  } else {
+    action.dataset.content = 'Invalid Password';
+  }
 });
